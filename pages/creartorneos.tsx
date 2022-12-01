726bb98fddecacabc51ea6d29f007123eb97e785
WeimarC <89791568+WeimarC@users.noreply.github.com>
2022-12-01 12:21:50 -0300

    modificación en el css de crear torneo

--- pages/creartorneos.tsx
+++ pages/creartorneos.tsx
@@ -18,24 +18,23 @@ const crearTorneo = () => {
     const { name } = router.query
     return (
         <>
-            <Typography>Crear torneo</Typography>
+            <div id="create">
+            <Typography id="title-1">Crear torneo</Typography>
             <Typography>{name}</Typography>
 
             <TextField id="outlined-basic" label="Nombre del equipo" variant="outlined" onChange={handleChange} />
             <Button onClick={handlePress}>agregar equipos</Button>
-            <select name="Tipo de torneo">
+            <select id='btn-select' name="Tipo de torneo">
 
                 <option value="value1" selected>-Seleccionar tipo de torneo-</option>
                 <option value="value1">Eliminatoria</option>
                 <option value="value2">Liga</option>
-                <option value="value3">Fase de grupos y Eliminatorias</option>
             </select>
-
+        <br />
             <a href='/mistorneos'>
-               <button>Crear torneo</button>
+               <button id="send">Crear torneo</button>
             </a>
-
-
+            </div>
         </>
     )
 }
